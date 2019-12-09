baedc7782eb75f103dd40f48df2b213d96df9b7d
An <tt50914@settour.com.uw>
2019-12-09 11:58:40 +0800

    package.json script update

--- src/components/app.js
+++ src/components/app.js
@@ -6,6 +6,7 @@ import styled from 'styled-components'
 const Container = styled.div`
   color: red;
   text-align: center;
+  opaciy: 1;
 `
 
 class App extends React.Component {
@@ -14,6 +15,7 @@ class App extends React.Component {
       <Container>
         <div className="app">webpack 4 環境建制1</div>
       </Container>
+      // npm run git -- "" && postgit
     );
   }
 }
