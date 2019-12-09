9e3ece1344f63e8dc734a1569cdc4a1eec9c137d
An <tt50914@settour.com.uw>
2019-12-09 11:42:13 +0800

    update styled components

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import styled from 'styled-components'
+
+const Container = styled.div`
+  color: red;
+  text-align: center;
+`
+
 class App extends React.Component {
   render() {
     return (
-      <div className="app">webpack 4 環境建制</div>
+      <Container>
+        <div className="app">webpack 4 環境建制1</div>
+      </Container>
     );
   }
 }
--- webpack.hot.config.js
+++ webpack.hot.config.js
@@ -3,6 +3,10 @@ const webpack = require('webpack');
 // https://juejin.im/post/5a10d9fe51882554bd50a5d3
 // express-hot-reload // https://github.com/kevinsimper/express-reload
 // webpack-hot-reload // https://medium.com/@Ahmad.Asaad/webpack-dev-server-hot-module-replacement-hmr-and-source-maps-easing-the-pain-e7cee99e3bdf
+// webpack4 sass & css https://medium.com/@mmaarriicckk/webpack4-sass-css-835062e31f6f
+// style-loader // 將css打包進js內,需要轉換字串所引用
+// css-loader // 將css轉換成CommonJS (es5前使用的模組化標準)
+// 處理sass轉換 // sass-loader-將sass轉成css編輯方式,而node-sass,則可以透過.sass轉成.css
 
 module.exports = {
   mode: 'development',
