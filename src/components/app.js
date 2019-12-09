2feace2834a043026ed6cabf2fc48095e85744da
An <tt50914@settour.com.uw>
2019-12-09 14:16:51 +0800

    update css module

--- src/components/app.js
+++ src/components/app.js
@@ -1,19 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
 const Container = styled.div`
-  color: red;
+  color: ${props => props.primary ? 'green' : 'red'};
   text-align: center;
   opaciy: 1;
+  ${props => props.primary && css`
+    background: white;
+    border: 1px solid black;
+  `};
 `
 
 class App extends React.Component {
   render() {
     return (
-      <Container>
+      <Container prefetch>{/* prefetch預取 提前完成渲染? */}
         <div className="app">webpack 4 環境建制1</div>
+        <Container primary>
+          <div>test</div>
+        </Container>
       </Container>
       // npm run git -- "" && postgit
     );
