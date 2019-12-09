fb3e280dd45583660b9dc1c36d3a0b07dcd5a5d6
An <tt50914@settour.com.uw>
2019-12-09 11:26:14 +0800

    update hot-reload

--- webpack.hot.config.js
+++ webpack.hot.config.js
@@ -1,14 +1,17 @@
 const path = require('path');
 const webpack = require('webpack');
 // https://juejin.im/post/5a10d9fe51882554bd50a5d3
+// express-hot-reload // https://github.com/kevinsimper/express-reload
+// webpack-hot-reload // https://medium.com/@Ahmad.Asaad/webpack-dev-server-hot-module-replacement-hmr-and-source-maps-easing-the-pain-e7cee99e3bdf
 
 module.exports = {
   mode: 'development',
-  context: path.resolve(__dirname, 'components'),
+  context: path.resolve(__dirname, 'src'),
   devtool: 'cheap-module-eval-source-map',
   entry: {
     app: [
-      './app.js'
+      './components/app.js',
+      'webpack-hot-middleware/client?reload=true'
     ]
   },
   output: {
