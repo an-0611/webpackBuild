c50ef6958b1c50404b227367edaa6e22cd7843f1
An <tt50914@settour.com.uw>
2019-12-11 11:17:30 +0800

    webpack build  fix

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './components/App'
+import App from './components/app'
 // entry can't use react e.g. class App extends component , can't read it
 
 ReactDOM.render(
